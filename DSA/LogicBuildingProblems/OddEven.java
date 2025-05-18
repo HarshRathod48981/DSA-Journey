82ee393cefdbdf800a05dfad660738599cf12513
Harsh Rathod <harshismyname48981@gmail.com>
2025-05-18 21:34:03 +0530

    logic building problems started

---
+++ DSA/LogicBuildingProblems/OddEven.java
@@ -0,0 +1,15 @@
+package LogicBuildingProblems;
+
+//Given a positive integer n, determine whether it is odd or even. Return true if the number is even and false if the number is odd.
+
+public class OddEven {
+
+    public static boolean isEven(int n){
+        return n % 2 == 0;
+    }
+
+    public static void main(String[] args) {
+        int n = 100;
+        isEven(n);
+    }
+}
