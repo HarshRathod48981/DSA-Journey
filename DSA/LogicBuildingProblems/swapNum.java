4d77b25610f1d9242d885aad497131b10de3881d
Harsh Rathod <harshismyname48981@gmail.com>
2025-05-20 12:29:09 +0530

    swap number

---
+++ DSA/LogicBuildingProblems/swapNum.java
@@ -0,0 +1,20 @@
+package LogicBuildingProblems;
+
+public class swapNum {
+    static void swap(int a, int b){
+        int temp = a;
+        a = b;
+        b = temp;
+        System.out.println("A = " +a);
+        System.out.println("B = " +b);
+
+
+    }
+
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        swap(a,b);
+
+    }
+}
