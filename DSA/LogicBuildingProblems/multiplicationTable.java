0e9d79ca8f3f20f497e42747ea962e950b092bb2
Harsh Rathod <harshismyname48981@gmail.com>
2025-05-20 11:39:26 +0530

    multiplicationTable question solved

---
+++ DSA/LogicBuildingProblems/multiplicationTable.java
@@ -0,0 +1,14 @@
+package LogicBuildingProblems;
+
+public class multiplicationTable {
+    static void table(int n){
+        for (int i = 0; i <= 10; i++) {
+            System.out.println(n+"*"+i+"="+n*i);
+        }
+    }
+    
+    public static void main(String[] args) {
+        int n = 5;
+        table(n);
+    }
+}
