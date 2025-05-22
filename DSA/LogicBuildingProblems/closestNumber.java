d051d014df4e0710bc5bd9c31f70c235682d6ada
Harsh Rathod <harshismyname48981@gmail.com>
2025-05-22 10:14:13 +0530

    swap number

---
+++ DSA/LogicBuildingProblems/closestNumber.java
@@ -0,0 +1,26 @@
+package LogicBuildingProblems;
+
+public class closestNumber {
+
+     static int closest(int n, int m){
+         int rem = n%m;
+         if(rem == 0){
+             return n;
+         }
+         int lower = n - rem;
+         int upper = n + (m - rem);
+
+         if (Math.abs(n - lower) <= Math.abs(n - upper)) {
+             return lower;
+         } else {
+             return upper;
+         }
+     }
+    public static void main(String[] args) {
+        int n = 13;
+        int m = 4;
+        int res = closest(n,m);
+        System.out.println(res);
+    }
+
+}
