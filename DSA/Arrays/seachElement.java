d505147c5fb48425ad4fe23cd516beea35a06d8c
Harsh Rathod <harshismyname48981@gmail.com>
2025-05-23 21:47:41 +0530

    Search element in Array

---
+++ DSA/Arrays/seachElement.java
@@ -0,0 +1,29 @@
+package Arrays;
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class searchElement
+{
+    public static void main (String[] args) throws java.lang.Exception
+    {
+        boolean found = false;
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int x = sc.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            int elements = sc.nextInt();
+            if ( elements == x){
+                found = true;
+            }
+        }
+        if (found){
+            System.out.println("YES");
+        }else{
+            System.out.println("NO");
+        }
+
+    }
+}
