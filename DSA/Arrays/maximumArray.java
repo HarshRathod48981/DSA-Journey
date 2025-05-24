ac090d1c17252a093825e5f7cbd9ea99b02c0777
Harsh Rathod <harshismyname48981@gmail.com>
2025-05-24 21:56:21 +0530

    Search element in Array

---
+++ DSA/Arrays/maximumArray.java
@@ -0,0 +1,47 @@
+package Arrays;
+import java.util.*;
+
+public class maximumArray {
+
+    static void method1(){
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while (sc.hasNext()){
+            int N = sc.nextInt();
+            int max = 0;
+            for (int i = 0; i < N; i++) {
+                int Arr = sc.nextInt();
+                if (max < Arr) {
+                    max = Arr;
+                }
+            }
+            System.out.println(max);
+        }
+        sc.close();
+    }
+
+    static void method2(){
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        for (int t = 0; t < T; t++) {
+
+        }{
+            int N = sc.nextInt();
+            int max = 0;
+            for (int i = 0; i < N; i++) {
+                int Arr = sc.nextInt();
+                if (max < Arr) {
+                    max = Arr;
+                }
+            }
+            System.out.println(max);
+        }
+        sc.close();
+    }
+
+    public static void main(String[] args) {
+
+        method1();
+    }
+
+}
