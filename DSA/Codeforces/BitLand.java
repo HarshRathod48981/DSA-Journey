ffb0a62de5a98d72ce6e1722eaf9e056b3adf86d
Harsh Rathod <harshismyname48981@gmail.com>
2025-05-30 18:09:08 +0530

    bit++ problem codeforces

---
+++ DSA/Codeforces/BitLand.java
@@ -0,0 +1,19 @@
+package Codeforces;
+import java.util.*;
+
+public class BitLand {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int start = 0;
+        while (N-->0){
+            String operation = sc.next();
+            if (operation.equalsIgnoreCase("++X") || operation.equalsIgnoreCase("X++")){
+                start++;
+            } else if (operation.equalsIgnoreCase("--X") || operation.equalsIgnoreCase("X--")) {
+                start--;
+            }
+        }
+        System.out.println(start);
+    }
+}
