c87d46e4d6955d464d8a329dbaf2391efe1a1918
Harsh Rathod <harshismyname48981@gmail.com>
2025-06-01 22:42:47 +0530

    fitness problem codeforces

---
+++ DSA/Codechef/fitness.java
@@ -0,0 +1,16 @@
+package Codechef;
+
+import java.util.Scanner;
+
+public class fitness {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int t = scanner.nextInt();
+
+        while (t-- > 0) {
+            int x = scanner.nextInt();
+            System.out.println(x*3);
+
+        }
+    }
+}
