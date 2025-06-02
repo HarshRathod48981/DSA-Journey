07f51582da94e0503ce4deaef3b7cbeb9c0d4fe4
Harsh Rathod <harshismyname48981@gmail.com>
2025-06-02 21:05:12 +0530

    find reminder problem codechef

---
+++ DSA/Codechef/findReminder.java
@@ -0,0 +1,20 @@
+package Codechef;
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
+public class findReminder {
+    public static void main (String[] args) throws java.lang.Exception {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            int rem = a % b;
+            System.out.println(rem);
+        }
+
+    }
+}
