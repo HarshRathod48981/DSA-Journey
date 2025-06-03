363a62defef326d79c03cd99b3f252a22c7ce4ff
Harsh Rathod <harshismyname48981@gmail.com>
2025-06-03 10:41:37 +0530

    chef & chocolate problem codechef

---
+++ DSA/Codechef/chefAndChocolate.java
@@ -0,0 +1,21 @@
+package Codechef;
+
+import java.util.Scanner;
+
+public class chefAndChocolate {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int t = scanner.nextInt();
+
+        while (t-- > 0) {
+            int x = scanner.nextInt();
+            int y = scanner.nextInt();
+            int z = scanner.nextInt();
+            int total = x*5 + y*10;
+            int num = total/z;
+            System.out.println(num);
+        }
+    }
+}
+
+
