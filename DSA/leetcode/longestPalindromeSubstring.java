3cd6f43e7d61ab739dfeeba09170fcbc995e13d3
Harsh Rathod <harshismyname48981@gmail.com>
2025-06-10 21:33:33 +0530

    leetcode palindrome not compelete though

---
+++ DSA/leetcode/longestPalindromeSubstring.java
@@ -0,0 +1,24 @@
+package leetcode;
+
+
+/*class Solution {
+    public String longestPalindrome(String s) {
+        int start = 0;
+        int end = s.length() - 1;
+        while (start > end) {
+            if (s.charAt(start) != s.charAt(end)){
+                return
+            }
+        }
+        return
+    }
+
+}*/
+
+public class longestPalindromeSubstring {
+    public static void main(String[] args) {
+        String s = "babad";
+        //Solution sol = new Solution();
+        //sol.longestPalindrome(s);
+    }
+}
