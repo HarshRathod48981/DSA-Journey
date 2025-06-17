377f01566ef629de77e98034ed81fa9da3ebc983
Harsh Rathod <harshismyname48981@gmail.com>
2025-06-17 08:31:00 +0530

    2d array

---
+++ DSA/LogicBuildingProblemsGFG/arrays.java
@@ -0,0 +1,27 @@
+package LogicBuildingProblemsGFG;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class arrays {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        //2d array
+        int arr[][] = new int[3][3];
+        for (int i = 0; i <arr.length ; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                arr[i][j] = sc.nextInt();
+            }
+        }
+
+        for (int i = 0; i <arr.length ; i++) {
+            System.out.println(Arrays.toString(arr[i]));
+        }
+
+
+
+
+    }
+}
