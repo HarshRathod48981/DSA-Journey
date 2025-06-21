d01f7ef27a7186e5a0a7aeb97e93a55eb1dd7eb5
Harsh Rathod <harshismyname48981@gmail.com>
2025-06-21 21:04:52 +0530

    arrays Q1 - swap elements

--- DSA/DSA_kunal_kushwaha/arrays.java
+++ DSA/DSA_kunal_kushwaha/arrays.java
@@ -10,20 +10,9 @@ public class arrays {
         int arr[] = {1,2,3,4,5};
 
         //Q1 - swap elements in array
-        //swap(arr,0,4);
-        //System.out.println(Arrays.toString(arr));
+        swap(arr,0,4);
+        System.out.println(Arrays.toString(arr));
 
-        //Q2 - max in array
-        //int resultMax = max(arr);
-        //System.out.println(resultMax);
-
-        //Q3 - min in array
-        int resultMin = min(arr);
-        System.out.println(resultMin);
-
-        //Q4 reverse an array
-        //reverse(arr);
-        //System.out.println(Arrays.toString(arr));
 
     }
 
@@ -33,35 +22,5 @@ public class arrays {
         arr[b] = temp;
     }
 
-    static int max(int arr[]){
-        int max = arr[0];
-        for (int i = 1; i < arr.length; i++) {
-            if (max < arr[i]){
-                max = arr[i];
-            }
-        }
-        //System.out.println(max);
-        return max;
-    }
 
-    static int min(int arr[]){
-        int min = arr[0];
-        for (int i = 1; i < arr.length; i++) {
-            if (arr[i] < min){
-                min = arr[i];
-            }
-        }
-        return min;
-
-    }
-
-    static void reverse(int arr[]){
-        int start = 0;
-        int end = arr.length-1;
-        while (start<end){
-            swap(arr,start,end);
-            start++;
-            end--;
-        }
-    }
 }
