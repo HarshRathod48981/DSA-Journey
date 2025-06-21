ed209e9f03fc27d8347cbd10a32951412647aec4
Harsh Rathod <harshismyname48981@gmail.com>
2025-06-21 21:12:38 +0530

    arrays Q2 - Max in array

--- DSA/DSA_kunal_kushwaha/arrays.java
+++ DSA/DSA_kunal_kushwaha/arrays.java
@@ -14,12 +14,27 @@ public class arrays {
         System.out.println(Arrays.toString(arr));
 
 
+        //Q2 - max in array
+        int resultMax = max(arr);
+        System.out.println(resultMax);
+
     }
 
     static void swap(int arr[], int a, int b){
         int temp = arr[a];
         arr[a] = arr[b];
         arr[b] = temp;
+    }
+
+    static int max(int arr[]){
+        int max = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (max < arr[i]){
+                max = arr[i];
+            }
+        }
+        //System.out.println(max);
+        return max;
     }
 
 
