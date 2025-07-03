b22904a4aeebbb04b670974919b2da6bc23680c4
Harsh Rathod <harshismyname48981@gmail.com>
2025-07-03 09:32:57 +0530

    binary search Q1 - part II ( order agnostic-binary search )

--- DSA/DSA_kunal_kushwaha/BinarySearch.java
+++ DSA/DSA_kunal_kushwaha/BinarySearch.java
@@ -3,23 +3,36 @@ package DSA_kunal_kushwaha;
 public class BinarySearch {
     public static void main(String[] args){
 
-        int arr[] = {2,4,6,8,10,12,14,16,18,20};
-        int result = binarySearch(arr,18);
+        int arr[] = {20,18,16,15,14,12,10,8,6,4,2};
+        int result = binarySearchAccending(arr,12);
         System.out.println(result);
 
     }
 
-    static int binarySearch(int[] arr, int target){
+
+    static int binarySearchAccending(int[] arr, int target){
         int start = 0;
         int end = arr.length -1;
+        boolean isAccending = arr[start] < arr[end];
+
         while(start <= end) {
             int mid = start + (end - start) / 2;
-            if (arr[mid] == target){
+            if (arr[mid] == target) {
                 return 1;
-            }else if (target > arr[mid]){
-                start = mid+1;
-            }else {
-                end = mid-1;
+            }
+            if (isAccending){
+                if (target > arr[mid]){
+                    start = mid+1;
+                }else {
+                    end = mid-1;
+                }
+            }
+            else{
+                if (target > arr[mid]){
+                    end = mid - 1;
+                }else{
+                    start = mid+1;
+                }
             }
         }
         return -1;
